53679bcdbca55ffa57e40ff8ca3a4cc6f4948db2
Triceracop <guykoy123@gmail.com>
2017-08-26 13:18:35 +0300

    Update os_walkTest.py

--- os_walkTest.py
+++ os_walkTest.py
@@ -1,6 +1,8 @@
+""" this will program demostarates the os.walk() function
+it will run through all the folders, subfolders and files in a given directory"""
 import os
-for folderName, subfolders, filenames in os.walk('C:\\emu8086'):
-    print('The current folder is ' + folderName)
+for folderName, subfolders, filenames in os.walk('C:\\exampleFolder'):
+    print('The current folder is ' + folderName)    #os.walk() will return lists of the folders,subfolders and files on which the for loop will itterate
     for subfolder in subfolders:
         print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
     for filename in filenames:
